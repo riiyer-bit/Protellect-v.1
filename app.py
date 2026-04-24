c87dfba915c83e73b1ca0e4f9508bb2bf6ef05d9
riiyer-bit <riiyer@ucsd.edu>
2026-04-23 19:10:15 -0700

    Change applymap to map for priority coloring

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ with left:
     styled = (
         scored_df[display_cols]
         .style
-        .applymap(color_priority, subset=["priority"])
+        .map(color_priority, subset=["priority"])
         .format({"normalized_score": "{:.3f}"})
         .set_properties(**{"font-size": "0.82rem"})
     )
